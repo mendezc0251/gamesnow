7509eef94d9a088c7f6cfee494489160594ef2ec
mendezc0251 <cmendez0251@gmail.com>
2025-11-03 16:38:38 -0600

    added ollama llm

--- ollama_prompt.py
+++ ollama_prompt.py
@@ -1,8 +1,10 @@
 import subprocess, json
 
 def call_llm(prompt):
-    result = subprocess.run(
-        ["ollama", "run", "llama3", prompt],
-        capture_output=True, text=True
-    )
+    
+    result = subprocess.run([
+    r"C:\Users\Cris\AppData\Local\Programs\Ollama\ollama.exe",
+    "run", "llama3", prompt
+], capture_output=True, text=True)
+    
     return result.stdout
